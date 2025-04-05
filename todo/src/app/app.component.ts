0f18e55598c08f18a7939bc66a8aa68cc6955b85
Cynthia Sosa <cynthiasosa33@iclould.com>
2025-04-04 21:26:16 -0400

    change

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,45 +1,32 @@
-
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Item } from './item';
-import { ItemComponent } from './item/item.component';
-
+import { Component } from "@angular/core";
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, ItemComponent],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: "app-root",
+templateUrl: "./app.component.html",
+styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'todo';
+title = "todo";
+filter: "all" | "active" | "done" = "all";
 
-  filter: 'all' | 'active' | 'done' = 'all';
-
-  allItems: Item[] = [
-    { description: 'eat', done: true },
-    { description: 'sleep', done: false },
-    { description: 'play', done: false },
-    { description: 'laugh', done: false }
+allItems = [
+  { description: "eat", done: true },
+  { description: "sleep", done: false },
+  { description: "play", done: false },
+  { description: "laugh", done: false },
   ];
-
   get items() {
-    if (this.filter === 'all') {
-      return this.allItems;
-    }
-    return this.allItems.filter((item) =>
-      this.filter === 'done' ? item.done : !item.done
-    );
+  if (this.filter === "all") {
+  return this.allItems;
   }
-
+  return this.allItems.filter((item) =>
+  this.filter === "done" ? item.done : !item.done
+  );
+  }
   addItem(description: string) {
-    this.allItems.unshift({
-      description,
-      done: false
-    });
+  this.allItems.unshift({
+  description,
+  done: false
+  });
   }
-
-  remove(item: Item) {
-    this.allItems.splice(this.allItems.indexOf(item), 1);
   }
-}
+  
